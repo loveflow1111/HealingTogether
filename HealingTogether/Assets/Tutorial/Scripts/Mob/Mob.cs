42ee5ed8e4c1571e7a391906e9f4d01a543481b7
loveflow1111 <minzoo8888@naver.com>
2025-02-28 23:30:28 +0900

    by 3/5
    -forward

--- HealingTogether/Assets/Tutorial/Scripts/Mob/Mob.cs
+++ HealingTogether/Assets/Tutorial/Scripts/Mob/Mob.cs
@@ -5,7 +5,14 @@ using UnityEngine.AI;
 
 public class Mob : MonoBehaviour
 {
+
+    public float destroyDelay = 1f;
+    private bool isDestroyed = false;
     private NavMeshAgent agent;
+
+    public ParticleSystem destroyParticle;
+    public AudioSource destroyAudio;
+    public GameObject modelGameObject;
 
     private void Awake()
     {
@@ -16,5 +23,26 @@ public class Mob : MonoBehaviour
     {
         agent.SetDestination(new Vector3(0f, 2f, 1f));
         agent.speed *= Random.Range(0.8f, 1.5f);
+
+        Invoke(nameof(Destroy), 3f); // 생성 후 3초 후에 자폭
+    }
+
+    public void Destroy()
+    {
+        //이미 파괴되어 있으면 무시
+        if (isDestroyed)
+            return;
+
+        isDestroyed = true;
+
+        destroyParticle.Play();
+        destroyAudio.Play();
+
+        //environmentParticle.Stop();
+        agent.enabled = false;
+        modelGameObject.SetActive(false);
+
+        Destroy(modelGameObject, destroyDelay);
+
     }
 }
